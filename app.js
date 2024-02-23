dbd3915fe5b2541167df23f19324c0f8094dca6f
Savio Lima <saviomachadolima2003@gmail.com>
2024-02-23 13:22:35 -0300

    Refinar site

--- app.js
+++ app.js
@@ -4,20 +4,33 @@ function sortear(){
     let quantidade = parseInt(document.getElementById('quantidade').value);
     let menorNumero = parseInt(document.getElementById('de').value);
     let maiorNumero = parseInt(document.getElementById('ate').value);
-   
-    let numerosArmazenados= [];
-    let geraNumero;
-    
-    for (let i = 0; i < quantidade; i++) {
-      do {geraNumero = gerarNumeroAleatorio(menorNumero , maiorNumero);
-      } while (numerosArmazenados.includes(geraNumero)); 
-        numerosArmazenados.push(geraNumero);
-    }
-    
-    let textoResultado = document.getElementById('resultado');
-    textoResultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${numerosArmazenados}</label>`;
 
-    alterarStatusBotao();
+    if (menorNumero >= maiorNumero) {
+      alert('Campo "Do número" deve ser inferior ao campo "Até o número". Verifique!');
+      habilitaBotãoReiniciar();
+      return;
+    }
+   
+    if(quantidade > maiorNumero - menorNumero + 1) {
+      alert(`Não é possivel realizar o sorteio: Não existe ${quantidade} números no intervalo de ${menorNumero} até ${maiorNumero}. Vertifique!`)
+    } else {
+      let numerosArmazenados= [];
+      let numeroGerado;
+      for (let i = 0; i < quantidade; i++) {
+       do {numeroGerado = gerarNumeroAleatorio(menorNumero , maiorNumero);
+         } while (numerosArmazenados.includes(numeroGerado)); 
+        numerosArmazenados.push(numeroGerado);
+      }
+      let numerosOrdenados = numerosArmazenados.slice().sort(function(a, b) {
+        return a - b;
+      });
+      
+      let palavrasNumerosSorteados = quantidade > 1 ? 'Números sorteados' : 'Número sorteado'
+      let textoResultado = document.getElementById('resultado');
+      textoResultado.innerHTML = `<label class="texto__paragrafo">${palavrasNumerosSorteados}: ${numerosOrdenados}</label>`;
+    } 
+
+    habilitaBotãoReiniciar();
 }
 
 
@@ -27,11 +40,9 @@ function reiniciar() {
   document.getElementById('ate').value = ' ';
   document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados: nenhum até agora</label>';
   
-  if (botao.classList.contains('container__botao')){
-    botao.classList.remove('container__botao');
-    botao.classList.add('container__botao-desabilitado');
-  }
+  desabilitaBotãoReiniciar();
 }
+
 
 
 
@@ -40,13 +51,19 @@ function gerarNumeroAleatorio(min , max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function alterarStatusBotao() {
+function habilitaBotãoReiniciar() {
   if (botao.classList.contains('container__botao-desabilitado')) {
-          botao.classList.remove('container__botao-desabilitado');
-          botao.classList.add('container__botao');
+    botao.classList.remove('container__botao-desabilitado');
+    botao.classList.add('container__botao');
   } 
 }
 
+function desabilitaBotãoReiniciar() {
+  if (botao.classList.contains('container__botao')){
+    botao.classList.remove('container__botao');
+    botao.classList.add('container__botao-desabilitado');
+  }
+}
 
   
 
